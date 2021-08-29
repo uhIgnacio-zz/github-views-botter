3455f46dfa5bac9efeb29f13c9942bf84bbe105a
xeny <44981148+1x6@users.noreply.github.com>
2021-08-29 15:12:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import requests
+import requests, threading
 
 url = input('raw link: ')
-sent = 0
+amount = input('enter the amount of threads (go for 300): ')
 
-while True: 
-    requests.get(url)
-    sent += 1
-    print(f'Views sent: %s' % sent)
+def trol():
+  sent = 0
+  while True:    
+      requests.get(url)
+      sent += 1
+      print(f'Views sent: %s' % sent)
+
+for _ in range(int(amount)):
+    t = threading.Thread(target=trol)
+    t.start()

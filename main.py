687125af5952a0e1f23035132554646cb37da1fa
igna <88955484+nekospara@users.noreply.github.com>
2021-09-04 05:53:27 -0400

    ye

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ url = os.environ['URL']
 amount = os.environ['AMOUNT']
 
 def thread():
-  sent = 0
-  while True:    
-      requests.get(url)
+  while True:  
+     sent = 0
+     r = requests.get(url)
+     if r.status_code == 403:
+         print('Invalid URL')
+     else:
+      pass
       sent += 1
       print(f'Views sent: %s' % sent)
 

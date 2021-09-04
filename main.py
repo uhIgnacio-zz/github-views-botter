e00f47160e23de32f4ca33d7b861f8567c9bf7d0
igna <88955484+nekospara@users.noreply.github.com>
2021-09-04 05:52:06 -0400

    few stuff

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import requests, threading
+import requests, threading, os
 
 url = input('raw link: ')
 amount = input('enter the amount of threads (go for 300): ')
-sent = 0
 
 def thread():
-  global sent
-  while True:    
-      requests.get(url)
+  while True:  
+     sent = 0
+     r = requests.get(url)
+     if r.status_code == 403:
+         print('Invalid URL')
+     else:
+      pass
       sent += 1
       print(f'Views sent: %s' % sent)
 
